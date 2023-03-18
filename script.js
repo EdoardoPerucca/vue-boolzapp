c9232dec6b3cfb0bd0f9e0d5378c9736c524f816
EdoardoPerucca <edoardo.perucca@hotmail.com>
2023-03-18 17:40:32 +0100

    search input in progress

--- script.js
+++ script.js
@@ -3,8 +3,29 @@ Milestone 1
 Replica della grafica con la possibilità di avere messaggi 
 scritti dall’utente (verdi) e dall’interlocutore (bianco) 
 assegnando due classi CSS diverse
+
 Visualizzazione dinamica della lista contatti: tramite la 
 direttiva v-for, visualizzare nome e immagine di ogni contatto
+
+Milestone 2
+Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, 
+visualizzare tutti i messaggi relativi al contatto attivo all’interno 
+del pannello della conversazione
+
+Click sul contatto mostra la conversazione del contatto cliccato
+
+Milestone 3
+Aggiunta di un messaggio: l’utente scrive un testo nella parte 
+bassa e digitando “enter” il testo viene aggiunto al thread sopra, 
+come messaggio verde
+Risposta dall’interlocutore: ad ogni inserimento di un messaggio, 
+l’utente riceverà un “ok” come risposta, che apparirà dopo 1 secondo.
+
+Milestone 4
+Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati 
+solo i contatti il cui nome contiene le lettere inserite 
+(es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
 */
 
 const { createApp } = Vue
@@ -12,6 +33,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
+
+        search: '',
         
         contacts: [
           {
@@ -174,16 +197,49 @@ const { createApp } = Vue
                       status: 'received'
                   }
               ],
+             
           }
-      ]
+      ],
     
+      activeUserIndex: 0,
         
+      newMessage: '',
         
       }
-    }
+    },
 
 
-   
+    computed: {
+        
+        userFilterSearch() {
+            if (this.search.trim().lenght > 0) {
+                return this.contacts.filter((user) => user.name.includes (this.search.trim()))
+            }
+            return this.contacts
+
+        },
+    },
+    
+
+    methods: {
+        
+        // cambia indice user corrispondente al click
+        changeUserIndex(changeIndex) {
+            this.activeUserIndex = changeIndex;
+            
+    },
+
+    addNewMessage() {
+        this.contacts.push(this.newMessage);
+        console.log(this.newMessage)
+        
+    },
+    
+    
+    
+    
+    
+   }
 
 
 
